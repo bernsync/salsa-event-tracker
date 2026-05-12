67e51377f35997a28cff4f540f6ca885f33b19d8
berns-noam <noam.berns@gmail.com>
2026-05-12 19:41:06 -0400

    full scale UI updates

--- web/event-links.js
+++ web/event-links.js
@@ -32,18 +32,28 @@ window.eventLinks = {
     instagram: "@colognesalsacongress",
     notes: "Salsa, Bachata and Kizomba festival. Screenshot note: 22-24 May 2026; promo code INSTA10 saves 10 EUR; day and night socials; workshops in 3 rooms."
   },
-  "World Salsa Festival": {
-    website: "https://www.worldsalsafest.com/"
-  },
-  "Live 2 Mambo: 2 Weekends": {
+  "Live 2 Mambo: Novotel": {
     website: "https://thedancehouse.eu/",
     instagram: "@thedancehouse.events",
+    facebook: "https://www.facebook.com/thedancehouse.events",
     organizer: "The Dance House"
   },
-  "Amsterdam Salsa Weekender": {
+  "Live 2 Mambo: Carnival Days": {
     website: "https://thedancehouse.eu/",
     instagram: "@thedancehouse.events",
+    facebook: "https://www.facebook.com/thedancehouse.events",
     organizer: "The Dance House"
+  },
+  "Live 2 Mambo: New York Palace": {
+    website: "https://thedancehouse.eu/",
+    instagram: "@thedancehouse.events",
+    facebook: "https://www.facebook.com/thedancehouse.events",
+    organizer: "The Dance House"
+  },
+  "Amsterdam Salsa Weekend": {
+    website: "https://www.amsterdamsalsaweekend.com/",
+    organizer: "Gunes Diker",
+    notes: "Official event name is Amsterdam Salsa Weekend. 2025 edition ran March 21-24 in Amsterdam."
   },
   "Bucharest Salsa Revolution": {
     website: "https://bucharestsalsarevolution.com/",
@@ -95,16 +105,19 @@ window.eventLinks = {
   "Back 2 Mambo": {
     website: "https://thedancehouse.eu/",
     instagram: "@thedancehouse.events",
+    facebook: "https://www.facebook.com/thedancehouse.events",
     organizer: "The Dance House"
   },
   "The Dance Hub": {
     website: "https://thedancehouse.eu/",
     instagram: "@thedancehouse.events",
+    facebook: "https://www.facebook.com/thedancehouse.events",
     organizer: "The Dance House"
   },
   "Mambo y Nada Mas": {
     website: "https://thedancehouse.eu/",
     instagram: "@thedancehouse.events",
+    facebook: "https://www.facebook.com/thedancehouse.events",
     organizer: "The Dance House",
     venue: "FAUNIA MADRID",
     notes: "Program: https://thedancehouse.eu/program/madrid/ | Venue: FAUNIA MADRID, Av. de las Comunidades, 28, Vicalvaro, 28032 Madrid, Spain. The Dance House ticket page lists Madrid as 29-31 May 2026. Venue page says it is about 15 minutes by car from Atocha and mentions hourly shuttle service."
@@ -112,6 +125,7 @@ window.eventLinks = {
   "Mambo Italiano": {
     website: "https://thedancehouse.eu/",
     instagram: "@thedancehouse.events",
+    facebook: "https://www.facebook.com/thedancehouse.events",
     organizer: "The Dance House"
   },
   "Beat Passion Mambo": {
@@ -197,8 +211,8 @@ window.eventLinks = {
     instagram: "@zagrebsalsamarathon",
     facebook: "https://www.facebook.com/ZagrebSalsaMarathon",
     organizer: "Zagreb Salsa Marathon",
-    djs: "5 top DJs",
-    notes: "Official source/screenshot notes: 5 top DJs, 3 night parties, 2 salsa & chill afternoon socials, 1 salsa picnic. 4th edition listed as 30 April-2 May 2026; Facebook result also shows third edition 24-26 April 2026."
+    djs: "Martina, Dmitri, Sergio, Gasper, Adri",
+    notes: "2026 edition listed for April 24-26 in Zagreb. Program notes include 3 night parties, 2 salsa and chill afternoon socials, 1 salsa picnic, and 5 DJs."
   },
   "Brussels Salsa Marathon": {
     website: "https://bybrussels.com/event/brussels-salsa-marathon-edition-08/",
@@ -292,6 +306,167 @@ window.eventLinks = {
     organizer: "Dance Marathon Germany",
     venue: "Stuttgart, Germany",
     price: "Full pass 169 EUR; full pass hotel 449-549 EUR; day pass 75 EUR; party pass 119 EUR",
+    djs: "Dmitri, Tuli, Rumbero, Julian the Duke, Michelle, Sergio",
+    artists: "Alicia Velasco, Victor Ortega, AlexiStyle",
+    notes: "2nd Dance Marathon Stuttgart, 31 July-3 August 2026. Around 40 hours of dancing, 3 afternoon socials and main night parties, workshops, coffee/water/cookies and light snacks for full pass holders."
+  }
+};
+
+window.eventEditionDetails = {
+  "amsterdam salsa weekend|2025-03-21": {
+    notes: "Official event name is Amsterdam Salsa Weekend. 2025 edition ran March 21-24 in Amsterdam."
+  },
+  "live 2 mambo: novotel|2026-02-05": {
+    venue: "Novotel Budapest City & Budapest Congress Center",
+    notes: "Official 2026 program calls this the First Weekend / Ultimacy. The Dance House notes that this weekend is in Novotel."
+  },
+  "live 2 mambo: carnival days|2026-02-09": {
+    venue: "Museum of Ethnography, Budapest",
+    notes: "Official 2026 program labels the weekdays between the weekends as Carnival. The Dance House notes that bootcamps and Carnival parties are in the Museum of Ethnography, with the last bootcamp day at Anantara New York Palace."
+  },
+  "live 2 mambo: new york palace|2026-02-12": {
+    venue: "Anantara New York Palace Budapest Hotel",
+    notes: "Official 2026 program calls this the Second Weekend / Society. The Dance House notes that this weekend is in New York Palace."
+  },
+  "berlin salsa congress|2026-08-28": {
+    price: "Romantic Choreo Bootcamp: 90 EUR early bird, 100 EUR regular, 120 EUR last minute",
+    artists: "Nando, Margarita",
+    notes: "2026 Jungle Vibes Edition. Romantic Choreo Bootcamp is separate from regular festival workshops."
+  },
+  "big 3 festival|2027-02-19": {
+    venue: "CCH Hamburg",
+    notes: "Source note: CCH Hamburg / Radisson Blu area, all-in-one venue."
+  },
+  "big milan|2027-03-25": {
+    venue: "Grand Hotel Barone di Sassj, Sesto San Giovanni"
+  },
+  "cologne salsa congress|2026-05-22": {
+    notes: "Salsa, Bachata and Kizomba festival. Screenshot note: 22-24 May 2026; promo code INSTA10 saves 10 EUR; day and night socials; workshops in 3 rooms."
+  },
+  "5star congress|2026-05-01": {
+    venue: "Radisson Blu Hotel London Heathrow",
+    price: "Weekend Marathon Party Pass from GBP 107; Full Pass from GBP 127",
+    currency: "GBP",
+    djs: "Julian The Duke, Javier La Rosa, Julian Summers, Tuli, Rupert Boyle, Erick The Saint, Amanda Hallam",
+    notes: "Annual London salsa congress by Mambo City with workshops, shows, social dancing, and marathon party pass options."
+  },
+  "hamburg salsa weekend|2026-07-24": {
+    venue: "MBA24, Max-Brauer-Allee, 22765 Hamburg",
+    notes: "Second edition. Official page describes more dance instructors, top DJs and one weekend of pure salsa energy."
+  },
+  "salsarave by cobeatparty|2026-09-10": {
+    venue: "Santa Susanna, Costa Brava",
+    notes: "Formerly connected to CoBeatParty Live. Screenshot note: September 10-13, 2026 in Barcelona/Spain; 900-1000 dancers from 40+ countries; tickets, line-up and updates on salsarave.com."
+  },
+  "paris salsa marathon|2026-09-18": {
+    venue: "Novotel Paris-Est",
+    notes: "Paris Salsa Marathon On2. 5th edition / 5-year anniversary. Screenshot confirms September 18-20, 2026."
+  },
+  "prague salsa marathon|2026-05-07": {
+    venue: "Fanta building, Main Train Station, Wilsonova 300/8, 11000 Prague 1, Czechia",
+    price: "Standard pass 238 EUR",
+    currency: "EUR",
+    djs: "DJ Julian, DJ Anael, DJ Morena, DJ Cycy, DJ V, DJ Czequita, DJ Randy, DJ Koki",
+    notes: "100% social marathon: no shows, no workshops. All-inclusive food and soft drinks. May 2026 edition includes Romantic Grotta meetup and Fanta building marathon venue."
+  },
+  "porto salsa weekend|2026-10-02": {
+    venue: "Heranca Magna, R. de Serpa Pinto 239, 4400-307 Vila Nova de Gaia, Portugal",
+    notes: "Official page lists October 2-4, 2026. 5th edition; socials and parties only."
+  },
+  "mambo y nada mas|2026-05-29": {
+    venue: "FAUNIA MADRID",
+    notes: "Program: https://thedancehouse.eu/program/madrid/ | Venue: FAUNIA MADRID, Av. de las Comunidades, 28, Vicalvaro, 28032 Madrid, Spain. The Dance House ticket page lists Madrid as 29-31 May 2026. Venue page says it is about 15 minutes by car from Atocha and mentions hourly shuttle service."
+  },
+  "beat passion mambo|2026-11-06": {
+    venue: "M Hotel, Derceva ulica 4, Ljubljana, Slovenia",
+    notes: "Official page lists 6-9 November 2026 in Ljubljana. All-in-one venue at M Hotel; main event Friday around 4 PM to Monday 6 AM."
+  },
+  "mambo bergen weekend|2027-03-12": {
+    venue: "Scandic Bergen City"
+  },
+  "salsa grand|2026-11-27": {
+    venue: "Sheraton Hotel, Zagreb",
+    notes: "Sophisticated Salsa & Mambo festival in Zagreb by Sea Sun Salsa. Screenshot confirms Hotel Sheraton and 27-29 November 2026."
+  },
+  "croatia summer salsa festival|2026-06-08": {
+    venue: "Rovinj, Croatia",
+    artists: "200+ workshops; salsa on1/on2, Cuban, Afro-Cuban, bachata and more",
+    notes: "22nd edition. Official page lists 08-15 June 2026, 5,000+ dancers from 80+ countries, beach/boat parties, open-air socials and live concerts."
+  },
+  "baila new york|2026-06-19": {
+    venue: "450 W 31st St, New York, NY 10001",
+    djs: "International and local DJs",
+    artists: "Afro-Caribbean, Cuban, Mambo/Salsa, Bachata, Konpa and Kizomba artists",
+    notes: "Afro-Caribbean Dance Fest with four parallel rooms: Cuban, Mambo/Salsa, Bachata, Konpa/Kizomba. Includes live music, workshops, socials, rooftop/pre/after parties."
+  },
+  "chocolate mambo marathon|2026-06-26": {
+    venue: "MusicContainer, Uster, Switzerland",
+    price: "185 / 195 CHF",
+    currency: "CHF",
+    djs: "6 DJs",
+    notes: "Official page lists June 26-28, 2026 in Uster, Switzerland. All-inclusive, small friendly marathon focused on social dancing; Calle Vapor concert on June 25 is separate."
+  },
+  "vivaz|2026-07-16": {
+    notes: "Vivaz Salsa Weekend. Screenshot note: July 16-19; official invitation mentions Bucharest Salsa Revolution."
+  },
+  "perfectly mambo|2026-08-21": {
+    notes: "Geneva-based association organizing and promoting mambo events. Perfectly Mambo Geneva Weekend, 3rd edition, August 21-23, 2026."
+  },
+  "el sol|2026-11-12": {
+    notes: "All-in-one venue dance festival in Warsaw. Screenshot confirms 12-16 November 2026 and buy-pass page."
+  },
+  "zagreb salsa marathon|2026-04-24": {
+    djs: "Martina, Dmitri, Sergio, Gasper, Adri",
+    notes: "2026 edition listed for April 24-26 in Zagreb. Program notes include 3 night parties, 2 salsa and chill afternoon socials, 1 salsa picnic, and 5 DJs."
+  },
+  "amsterdam international salsa festival|2027-02-26": {
+    venue: "De Koning Party & Event Centre"
+  },
+  "amsterdam international salsa marathon|2026-10-16": {
+    venue: "CanDance Studios",
+    notes: "Amsterdam International Salsa Marathon by Extremos. Screenshot note: 16-18 October 2026, three days of Bachata and Salsa workshops and social dancing."
+  },
+  "nysbkz|2027-01-29": {
+    venue: "New York Hilton Midtown",
+    notes: "New York SBKZ Congress: Salsa, Bachata, Kizomba, Zouk. Screenshot confirms Jan 29-Feb 1, 2027 and Hilton Midtown venue."
+  },
+  "mambo marathonios|2027-04-21": {
+    venue: "Royal & Imperial Belvedere Resort, Hersonissos, Crete"
+  },
+  "pink marathon|2026-10-23": {
+    notes: "Source note: Toulouse, France. 35h of music, 100% social dance, unlimited snacks and soft drinks. Tickets via Billetweb."
+  },
+  "smyrna mambo getaway|2026-05-21": {
+    notes: "Official page: 5th edition on 21-24 May 2026 in Izmir, Turkiye. Unique combination of mambo and early summer holiday."
+  },
+  "salsa king festival|2026-09-16": {
+    venue: "Kipriotis Village Resort, Kos Island, Greece",
+    notes: "Salsa holiday experience powered by Bachata King Festival. Screenshot note: 5-day event, 16-20 September 2026."
+  },
+  "salsa spring festival|2026-05-21": {
+    venue: "Club Hotel Casino Loutraki",
+    notes: "15th anniversary edition. Screenshot note: 21-24 May 2026; Salsa and Bachata Festival in Greece."
+  },
+  "brussels mambo weekend|2026-09-04": {
+    venue: "Chaussee de Zellik 65, 1082 Brussels Capital",
+    notes: "International Brussels Mambo Weekend. Screenshot note: 4-7 September 2026; salsa, mambo and bachata workshops, social dancing, shows, two dancefloors, top DJs, gala party. Tickets via Weezevent."
+  },
+  "empire sensual movement|2026-07-31": {
+    venue: "Marriott Marquis Times Square, New York",
+    notes: "Empire Sensual Movement Salsa & Bachata Congress. Screenshot confirms July 31-August 3, 2026 in NYC."
+  },
+  "london salsa marathon|2026-07-23": {
+    venue: "Porchester Hall, Porchester Rd, London W2 5HS, United Kingdom",
+    price: "Full weekend pass from GBP 99 + fee; couple pass GBP 180; day passes from GBP 65/75; workshops-only passes from GBP 50",
+    currency: "GBP",
+    djs: "DJ Willy, DJ Julian the Duke, DJ El Classico, DJ Sam Sleek, DJ Missy, DJ Tuli",
+    artists: "Grupo Alafia, Kat & Eddie, Super Mario, Moe Flex, Natasha Chuma, Fadi K, Andreez Arion, Esencialibre, Sam Mr. Sleek, Marchant & Davina, John & Yulia, Mambo Sirenas, Gaby & Razvan",
+    notes: "5 Year Anniversary Edition, 23-27 July 2026. Main dancing Friday-Sunday; Thursday pre-party and Monday after-party are not included with the main pass."
+  },
+  "dance marathon germany|2026-07-31": {
+    venue: "Stuttgart, Germany",
+    price: "Full pass 169 EUR; full pass hotel 449-549 EUR; day pass 75 EUR; party pass 119 EUR",
+    currency: "EUR",
     djs: "Dmitri, Tuli, Rumbero, Julian the Duke, Michelle, Sergio",
     artists: "Alicia Velasco, Victor Ortega, AlexiStyle",
     notes: "2nd Dance Marathon Stuttgart, 31 July-3 August 2026. Around 40 hours of dancing, 3 afternoon socials and main night parties, workshops, coffee/water/cookies and light snacks for full pass holders."
